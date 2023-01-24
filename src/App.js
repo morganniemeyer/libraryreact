558774cb16330ad7fe4d828c98d63eb4246fdc1a
Morgan Niemeyer <malineniemeyer@gmail.com>
2023-01-24 16:44:56 -0600

    router built and functional for home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import BookList from './components/book/BookList';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home/Home.js';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </main>
   );
 }
